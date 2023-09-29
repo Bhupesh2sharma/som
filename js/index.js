9a472116d36b285cca9742486119a479c7123f36
bhupesh <bhupeshxrma24233@gmail.com>
2023-09-29 17:07:30 +0530

    Final Commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function whatsapp() {
+    let arr = document.querySelector(".arr").value;
+    let dep = document.querySelector(".dep").value;
+    let adult = document.querySelector(".adult").value;
+    let child = document.querySelector(".child").value;
+    let rooms = document.querySelector(".rooms").value;
+    var i = true;
+    if (arr == "" || dep == "" || adult == "" || child == ""|| rooms == "") {
+      document.querySelector(".vr").innerHTML =
+        "<p>Enter the required field</p>";
+      document.querySelector(".vr").style = "color:red;";
+      i = false;
+    }
+    if (i) {
+      const url =
+        "https://wa.me/919733230418?text=" +
+        "arrival :" +
+        arr +
+        "%0a" +
+        "depature :" +
+        dep +
+        "%0a" +
+        "adult :" +
+        adult +
+        "%0a" +
+        "child :" +
+        child
+        "Rooms :" +
+        rooms;
+  
+      window.open(url, "_blank").focus();
+    }
+  }
+  
